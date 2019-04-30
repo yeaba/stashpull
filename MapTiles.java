3234e9b006fbcb5139d0504252ad11c6c6dc9c0e
yeaba <yeabkalassegid123@gmail.com>
2019-04-29 20:18:42 -0500

    Create MapTiles.java

---
+++ MapTiles.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class MapTiles {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] q = br.readLine().toCharArray();
+		ArrayList<Integer> quad = new ArrayList<>();
+		for (char c : q) {
+			quad.add(Integer.parseInt("" + c));
+		}
+		solveNextProblem(quad);
+	}
+
+	static void solveNextProblem(ArrayList<Integer> quadKey) {
+		int x = 0;
+		int y = 0;
+
+		for (int n : quadKey) {
+			x *= 2;
+			y *= 2;
+			x += n%2;
+			if (n > 1) {
+				y++;
+			}
+		}
+		
+		System.out.println("" + quadKey.size() + " " + x + " " + y);
+	}
+
+}
